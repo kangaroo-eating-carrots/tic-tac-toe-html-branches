5a73682e2b692bcce61cef48db89e39a1f9b8a14
20026216 <20026216@tafe.wa.edu.au>
2024-09-09 13:15:22 +0800

    created player stub, removed main.py

---
+++ tic-tac-toe-v2/src/player.py
@@ -0,0 +1,14 @@
+class Player:
+    def __init__(self, name, symbol, board=None):
+        self.name = name
+        self.symbol = symbol
+        self.board = board
+
+    # TODO: get user name and symbol
+    def start(self):
+        pass
+    # TODO: get rows and column where the player wants to put symbol
+    def user_input(self):
+        pass
+
+
