1e39fc9467781c93a4054cfdfb1c84dce1023fa7
20026216 <20026216@tafe.wa.edu.au>
2024-09-16 12:16:24 +0800

    test: add player make choice function test.

---
+++ tic-tac-toe-v2/test/test_player.py
@@ -0,0 +1,13 @@
+import unittest
+from src.game_player import Player
+
+class TestPlayer(unittest.TestCase):
+    def setUp(self):
+        self.player = Player(mark="X")
+        self.choices = []
+
+    # tests if the player puts the choice on the right index position on the 2d
+    def test_make_choice(self):
+        self.player.make_choice(2, 3)
+        self.assertEqual(self.player.choices, [[0,2]])
+
